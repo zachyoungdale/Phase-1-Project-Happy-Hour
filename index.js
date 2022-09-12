d7f41e614664d52dc4db6b37d52c563fb61f0839
Zachary Youngdale <zyoungdale@gmail.com>
2022-09-12 16:39:31 -0400

    added function for new cocktail form and also added global variables for the image, name, ingredients, and instruction placeholders.

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 fetch("http://localhost:3000/cocktails")
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+//Zach here. this code below should handle the submit form for the new cocktail.
+
+const detailImage = document.querySelector(".detail-image");
+const defaultName = document.querySelector(".name");
+const defaultIngredients = document.querySelector(".ingredient-list");
+const defaultInstructions = document.querySelector("#instructions-placeholder");
+
+function handleSubmit() {
+  const newCocktailForm = document.querySelector("#new-cocktail");
+  newCocktailForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newName = e.target.name.value;
+    const newImage = e.target.image.value;
+    const newIngredients = e.target.ingredients.value;
+    const newInstructions = e.target["new-instructions"].value;
+
+    const newCocktail = {
+      name: newName,
+      image: newImage,
+      ingredients: newIngredients,
+      instructions: newInstructions,
+    };
+
+    detailImage.src = newCocktail.image;
+    (defaultName.textContent = newCocktail.name),
+      (defaultIngredients.textContent = newCocktail.ingredients);
+    defaultInstructions.textContent = newCocktail.instructions;
+  });
+}
+handleSubmit();
